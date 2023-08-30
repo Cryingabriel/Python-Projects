4a0b26fb5ff763af204923d43bb2683a9f97f2d5
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2023-08-30 15:39:59 -0600

    Update Lunar Lander.py

--- Lunar Lander.py
+++ Lunar Lander.py
@@ -30,3 +30,13 @@ text4 = font.render('Vertical velocity:', False, (200, 20, 20))
 text5 = font.render(str(int(pvy)), 1 , (200, 20, 20))
 text6 = font.render('Height', False, (20, 20, 200))
 text4 = font.render(str(int(ypos)), 1 , (20, 20, 200))
+
+
+# Game Loop___________________________________________________________________________________________
+while Exit == False:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Exit = True
+    pygame.quit()
