664adf3ced886bda6e691101377757184a3d1f16
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2023-09-01 15:39:59 -0600

    Update Lunar Lander.py

--- Lunar Lander.py
+++ Lunar Lander.py
@@ -15,10 +15,13 @@ clock = pygame.time.Clock()
 xpos = 350
 ypos = 0
 pvx = 0
-pvy = -10
+pvy = .10
 isground = False
 Rocketon = False
 Crashed = False
+meft = False
+might = False
+mup = False
 
 # Font Variables
 pygame.font.init()
@@ -35,8 +38,97 @@ text7 = font.render(str(int(ypos)), 1 , (20, 20, 200))
 # Game Loop___________________________________________________________________________________________
 while Exit == False:
     clock.tick(60)
-
+    #replace with keys.get
+    meft = False
+    might = False
+    mup = False
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Exit = True
-    pygame.quit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                meft = True
+            if event.key == pygame.K_RIGHT:
+                might = True
+            if event.key == pygame.K_UP:
+                mup = True
+
+   
+   
+   
+   
+   #___________Physics Section_____________________________
+   
+   
+    if meft == True:
+        pvx -= 3
+    if might == True:
+        pvx += 3
+    else:
+        pvx = 0
+    if mup == True:
+        pvy -= 4.17/60
+        isground = False
+        Rocketon = True
+
+    else:
+        if isground == False:
+            pvy += 1.65/60
+        Rocketon = False
+
+
+    if isground == True and abs(pvy) > .5:
+        Crashed = True
+        xpos = 350
+        ypos = 0
+        pvx = 0
+        pvy = 0
+        isground = False
+    
+    if isground == True and abs(pvy) < .5:
+        Crashed = False
+        pvy = 0
+        pvx = 0
+    
+    if ypos >950:
+        isground = True
+        ypos = 950
+
+
+    #______________Update Section__________________________________________
+    #Updated Player
+    xpos += pvx
+    ypos += pvy
+    #Updated Printed Velocity
+    text2 = font.render(str("%.2f" %(pvy*-1)), 1, (0, 200, 200))
+    text5 = font.render(str("%.2f" %(pvy*-1)), 1, (200, 20, 20))
+    #Updated Printed Height
+    text6 = font.render('Height:', False, (20, 20, 200))
+    text7 = font.render(str(int(1000-pvy)), 1, (0, 200, 200))
+
+    
+#_________Render Section____________________________________
+    screen.fill((0,0,0))
+    if Crashed == True:
+        screen.blit(text3, (200,500))
+        pygame.display.flip()
+        pygame.time.wait(1000)
+    
+    if abs(pvy) < .5:
+        screen.blit(text1, (10, 10))
+        screen.blit(text2, (250, 10))
+    else:
+        screen.blit(text4, (10, 10))
+        screen.blit(text5, (250, 10))
+    screen.blit(text6, (10, 60))
+    screen.blit(text7, (150, 60))
+
+    pygame.draw.rect(screen, (200, 200 ,200), (xpos, ypos, 50 ,50))
+
+    pygame.display.flip()
+
+
+    
+pygame.quit()
